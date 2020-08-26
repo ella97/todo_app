67cdeb91452771332c8b30c9c25937a472fc41ab
Noella Mwanri <nuwymwanri@gmail.com>
2020-08-26 19:58:19 +0300

    creating categories_screen and home_screen and drawer_navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
